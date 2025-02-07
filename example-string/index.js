a8fa05bfecc3e8a068249ebe6ac4f4a69238cf52
carlosando <39626038+carlosando@users.noreply.github.com>
2025-02-07 14:47:08 -0300

    new file:   example-string/example-string.zip 	new file:   example-string/index.js 	deleted:    example-string/example-string.zip 	deleted:    example-string/index.js 	string-example/

---
+++ example-string/index.js
@@ -0,0 +1,17 @@
+exports.handler = async (event, context, callback) => {
+    console.log("## Context: " + JSON.stringify(context));
+    console.log("## Event is: " + JSON.stringify(event));
+
+    try {
+        let inputString = event.inputString || "";
+        let response = {
+            characterCount: inputString.length
+        };
+
+        console.log("Function completed: Returning: " + JSON.stringify(response));
+        return response;
+    } catch (error) {
+        console.error("Handler failed: " + error);
+        callback(error);
+    }
+};
